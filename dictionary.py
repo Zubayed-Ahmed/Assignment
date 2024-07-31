757a6625d3878bce8369581e9abe64bfa964f005
Zubayed-Ahmed <jubayedahmed2001@gmail.com>
2024-07-31 21:57:32 +0600

    New file added

---
+++ dictionary.py
@@ -0,0 +1,24 @@
+my_dict = {
+    "name": "Jhon",
+    "age": 25,
+    "gender": "Male"
+}
+
+print(my_dict['age'])
+
+
+#get method
+print(my_dict.get('age'))
+#keys
+print(my_dict.items())
+#Add operation
+my_dict["new_key"]="new_value"
+#update
+my_dict["age"]= 48
+#Extend
+second_dictionary={
+    "new_new_key":"new_new_value"
+}
+my_dict.update(second_dictionary)
+
+#new_dictionary = my_dict + second_dictionary
